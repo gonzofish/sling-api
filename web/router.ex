c0f86c6347037b5b6a77d3af595f46a31753e3ba
Matt Fehskens <matt.fehskens@gmail.com>
2017-06-03 21:15:44 -0400

    Putting auth in place

--- web/router.ex
+++ web/router.ex
@@ -5,7 +5,32 @@ defmodule Sling.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_sessions
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
+    plug Guardian.Plug.LoadResource
+  end
+
+  scope "/", Sling do
+    pipe_through :browser
+
+    get "/", PageController, :index
+  end
+
   scope "/api", Sling do
     pipe_through :api
+
+    post "/sessions", SessionController, :create
+    delete "/sessions", SessionController, :destroy
+    post "/sessions/refresh", SessionController, :refresh
+    resources "/users", UserController, only [ :create ]
   end
 end
